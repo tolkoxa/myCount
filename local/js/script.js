baa764aef456fd30da7d79aa5b268ad2151c9a7d
tolkoxa <tolkoxa@ya.ru>
2020-08-28 02:14:44 +0300

    New local components

--- local/js/script.js
+++ local/js/script.js
@@ -1,21 +1,20 @@
-Vue.component('app', {
-    template: '<div id="app" class="wrapper"> <Mainpage> </Mainpage></div>'
-})
-
-Vue.component('Mainpage', {
-    template: '<div class="container"><Pagetop></Pagetop><Start></Start><Pagebottom></Pagebottom></div>'
-})
-
-Vue.component('Start', {
-    template: '<div class="main"><div v-if="counts_all == 0"><p class = "text">Сервис myCount &#151; универсальный счётчик для чего угодно: стаканов выпитой воды, комплиментов коллегам или звонков потенциальным клиентам. Всё то, что вам действительно нужно считать.</p><p class = "text">Счётчиков может быть сколько угодно. Каждый раз, когда нужное действие выполнено & #151; просто нажми на кнопку &laquo;Добавить&raquo;</p></div><div v-else><Newcount v-on:send-arr="get-arr"></Newcount>{{ counts_all }}</div><ButtonNewCount></ButtonNewCount></div>'
-})
-
 Vue.component('Pagetop',{
     template: '<div class="page-top"><p class="logo-text">myCount</p></div>'
 })
 
+Vue.component('Startpage', {
+    template: '<div class="main"><p class ="text">Сервис myCount &#151; универсальный счётчик для чего угодно: стаканов выпитой воды, комплиментов коллегам или звонков потенциальным клиентам. Всё то, что вам действительно нужно считать.</p><p class ="text">Счётчиков может быть сколько угодно. Каждый раз, когда нужное действие выполнено &#151; просто нажми на кнопку &laquo;Добавить&raquo;</p><ButtonNewCount></ButtonNewCount></div>'
+})
 
-let app = new Vue({
+Vue.component('Pagebottom',{
+    template: '<div class="footer"><p class="footer-text">2020</p><p class="footer-text">Сделал <a class="footer-link" href="https://tolkoxa.ru">tolkoxa.ru</a></p></div>'
+})
+
+Vue.component('ButtonNewCount',{
+    template: '<button class="btn">Добавить счётчик</button>'
+})
+
+let vm = new Vue({
     el: '#app',
     data: {}
 })
